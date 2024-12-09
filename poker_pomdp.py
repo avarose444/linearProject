0ee593921d8ca8f46a755bb6bcd2fc2fbdfaf452
Lyali Shaikh <lyalishaikh@Lyalis-MBP.lan>
2024-12-08 21:53:13 -0500

    state and action spaces

--- poker_pomdp.py
+++ poker_pomdp.py
@@ -3,3 +3,23 @@ import mdptoolbox
 import matplotlib.pyplot as plt
 import pomdp_py
 
+#defining state space
+stages = ["preflop", "flop", "turn", "river"]
+hand_strengths = ["weak", "medium", "strong"]
+pot_sizes = ["small", "medium", "large"]
+
+states = [
+    (stage, hand, pot)
+    for stage in stages
+    for hand in hand_strengths
+    for pot in pot_sizes
+]
+
+#define action space
+actions = {
+    0: "fold",
+    1: "check_call",
+    2: "bet_raise"
+}
+
+print("Actions: ", actions)
