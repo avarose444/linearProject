22f9f8787613bf6fcf3c7cd31a3526761ef72c99
Lyali Shaikh <lyalishaikh@Lyalis-MBP.lan>
2024-12-08 22:44:39 -0500

    transitions

--- poker_pomdp.py
+++ poker_pomdp.py
@@ -28,10 +28,37 @@ board_strengths = ["neutral_board", "strong_board"]
 observed_pot_sizes = ["small_pot", "medium_pot", "large_pot"]
 
 observations = [
-    (opponent_action, board, pot)
-    for opponent_action in opponent_actions
+    (opp_action, board, pot)
+    for opp_action in opponent_actions
     for board in board_strengths
     for pot in observed_pot_sizes
 ]
 
-print("observations: ", observations)
+def transition_function(state, action):
+    stage, hand_strength, pot_size = state
+
+    stage_order = ["preflop", "flop", "turn", "river"]
+    next_stage = stage_order[stage_order.index(stage)+1] if stage != "river" else "river"
+
+    if action == "fold":
+        return{}
+    elif action == "check_call":
+        return {
+            (next_stage, hand_strength, pot_size): 0.8,
+            (next_stage, "medium", pot_size): 0.2 if hand_strength=="weak" else 0.1, 
+            (next_stage, "strong", pot_size): 0.1 if hand_strength!="strong" else 0.0,
+        }
+    elif action == "bet_raise":
+        next_pot_size = {"small": "medium", "medium": "large", "large": "large"}[pot_size]
+        return {
+            (next_stage, hand_strength, next_pot_size): 0.6,
+            (next_stage, "medium", next_pot_size): 0.3 if hand_strength == "weak" else 0.2,
+            (next_stage, "strong", next_pot_size): 0.1,
+        }
+    else: 
+        raise ValueError("Unknown action: {}".format(action))
+    
+transition_table = {}
+for state in states:
+    for action in actions.values():
+        transition_table[(state, action)] = transition_function(state, action)
